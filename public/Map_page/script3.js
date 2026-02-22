8cb2741571275276665210150d6b5ff2e75901bf
suvitom <suvitom@metropolia.fi>
2026-02-22 21:11:04 +0200

    tabs removed from the marker selection dialog

--- public/Map_page/script3.js
+++ public/Map_page/script3.js
@@ -58,13 +58,13 @@ const addTextToThePage = () => {
   const opcLabel = document.getElementById('opacityLabl');
   opcLabel.textContent = fi ? 'läpinäkyvyys' : 'transparency';
 
-  const likedDialog = document.getElementById('liked').children[0];
-  likedDialog.textContent = fi
-    ? 'Kirjaudu sisään tai rekisteröidy tallentaaksesi suosikkeja!'
-    : 'Log in or register to save your favorites!';
+  // const likedDialog = document.getElementById('liked').children[0];
+  // likedDialog.textContent = fi
+  //   ? 'Kirjaudu sisään tai rekisteröidy tallentaaksesi suosikkeja!'
+  //   : 'Log in or register to save your favorites!';
 
-  const dialogLogin = document.querySelector('#dialogLoginLink > span');
-  dialogLogin.textContent = fi ? 'Kirjaudu / Rekisteröidy' : 'Log in / Register';
+  // const dialogLogin = document.querySelector('#dialogLoginLink > span');
+  // dialogLogin.textContent = fi ? 'Kirjaudu / Rekisteröidy' : 'Log in / Register';
 
   setLoginLogoutBtn(fi);
 };
